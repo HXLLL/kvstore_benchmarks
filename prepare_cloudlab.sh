c8fb3f066cb8c918250afb17c5a7d41da24978d7
HXLLL <1150189655@qq.com>
2021-08-16 17:41:08 +0800

    Update prepare_cloudlab.sh

--- prepare_cloudlab.sh
+++ prepare_cloudlab.sh
@@ -1,1 +1,7 @@
-
+git clone https://github.com/efficient/rdma_bench
+echo 8192 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+bash -c "echo kernel.shmmax = 9223372036854775807 >> /etc/sysctl.conf"
+bash -c "echo kernel.shmall = 1152921504606846720 >> /etc/sysctl.conf"
+sysctl -p /etc/sysctl.conf
+apt update
+apt install memcached libnuma-dev
